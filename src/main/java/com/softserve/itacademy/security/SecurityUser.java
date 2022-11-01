91aa1d6f9368fd9f037817055b3673a9e08aa83f
Vladyslav <voitiukdev@gmail.com>
2022-11-01 21:33:27 +0200

    Add config for authentication form DB

--- src/main/java/com/softserve/itacademy/security/SecurityUser.java
+++ src/main/java/com/softserve/itacademy/security/SecurityUser.java
@@ -1,10 +1,11 @@
 package com.softserve.itacademy.security;
 
+import com.softserve.itacademy.model.User;
 import lombok.Data;
+import org.springframework.context.support.BeanDefinitionDsl;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import com.softserve.itacademy.model.User;
 
 import java.util.Collection;
 import java.util.List;
@@ -15,8 +16,14 @@ public class SecurityUser implements UserDetails {
     private final String email;
     private final String password;
     private final List<SimpleGrantedAuthority> authorities;
-    private final boolean isActive = true;
+    private final boolean isActive;
 
+    public SecurityUser(String email, String password, List<SimpleGrantedAuthority> authorities, boolean isActive) {
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+        this.isActive = isActive;
+    }
 
 
     @Override
@@ -54,9 +61,10 @@ public class SecurityUser implements UserDetails {
         return isActive;
     }
 
-//    public static UserDetails fromUser(User user){
+    public static UserDetails fromUser(User user){
 //        return new org.springframework.security.core.userdetails.User(
-//                user.getEmail(), user.getPassword(), user.getRole()
+//                user.getEmail(), user.getPassword(), true, true, true, true, user.getRole()
 //        );
-//    }
+        return null;
+    }
 }
